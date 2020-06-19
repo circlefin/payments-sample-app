965a6dfe841cc760a0143aa466e8852bd6ebd613
Tarun Kumar <tkumar@circle.com>
2020-06-19 07:47:50 -0400

    Fixing build error

--- pages/debug/wallets/payouts/create.vue
+++ pages/debug/wallets/payouts/create.vue
@@ -40,7 +40,7 @@
 <script lang="ts">
 import { Component, Vue } from 'nuxt-property-decorator'
 import { mapGetters } from 'vuex'
-import uuidv4 from 'uuid/v4'
+import { v4 as uuidv4 } from 'uuid'
 import RequestInfo from '@/components/RequestInfo.vue'
 import ErrorSheet from '@/components/ErrorSheet.vue'
 import { CreatePayoutPayload } from '@/lib/payoutsApi'
