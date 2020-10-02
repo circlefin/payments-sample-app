cf4760ba20cb832cd519fe6dd2ecea043605d398
Alan Nguyen <alannguyen@circle.com>
2020-10-02 16:22:57 -0400

    implement deposit addresses pages

---
+++ pages/debug/businessAccount/addresses/deposit/create.vue
@@ -0,0 +1,91 @@
+<template>
+  <v-layout>
+    <v-row>
+      <v-col cols="12" md="4">
+        <v-form>
+          <v-text-field v-model="formData.currency" label="Currency" />
+
+          <v-text-field v-model="formData.chain" label="Blockchain ID" />
+
+          <v-btn
+            depressed
+            class="mb-7"
+            color="primary"
+            :loading="loading"
+            @click.prevent="makeApiCall"
+          >
+            Make api call
+          </v-btn>
+        </v-form>
+      </v-col>
+      <v-col cols="12" md="8">
+        <RequestInfo
+          :url="requestUrl"
+          :payload="payload"
+          :response="response"
+        />
+      </v-col>
+    </v-row>
+    <ErrorSheet
+      :error="error"
+      :show-error="showError"
+      @onChange="onErrorSheetClosed"
+    />
+  </v-layout>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+import { mapGetters } from 'vuex'
+import { v4 as uuidv4 } from 'uuid'
+import RequestInfo from '@/components/RequestInfo.vue'
+import ErrorSheet from '@/components/ErrorSheet.vue'
+import { CreateDepositAddressPayload } from '@/lib/businessAccount/addressesApi'
+@Component({
+  components: {
+    RequestInfo,
+    ErrorSheet,
+  },
+  computed: {
+    ...mapGetters({
+      payload: 'getRequestPayload',
+      response: 'getRequestResponse',
+      requestUrl: 'getRequestUrl',
+      isMarketplace: 'isMarketplace',
+    }),
+  },
+})
+export default class CreateDepositAddressClass extends Vue {
+  formData = {
+    currency: '',
+    chain: '',
+  }
+
+  required = [(v: string) => !!v || 'Field is required']
+  error = {}
+  loading = false
+  showError = false
+  onErrorSheetClosed() {
+    this.error = {}
+    this.showError = false
+  }
+
+  async makeApiCall() {
+    this.loading = true
+    const { currency, chain } = this.formData
+    const payload: CreateDepositAddressPayload = {
+      idempotencyKey: uuidv4(),
+      currency,
+      chain,
+    }
+    try {
+      await this.$businessAccountAddressesApi.createDepositAddress(payload)
+    } catch (error) {
+      this.error = error
+      this.showError = true
+    } finally {
+      this.loading = false
+    }
+  }
+}
+</script>
