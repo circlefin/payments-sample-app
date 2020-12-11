3937fddb06f1844394669e9bdcbb686542a4ed69
tlin <tlin@circle.com>
2020-12-11 00:35:11 -0500

    Support ach payout

--- pages/debug/payouts/create.vue
+++ pages/debug/payouts/create.vue
@@ -15,6 +15,12 @@
             label="Fiat Account Id"
           />
 
+          <v-select
+            v-model="formData.destinationType"
+            :items="destinationType"
+            label="Fiat Account Type"
+          />
+
           <v-text-field
             v-model="formData.beneficiaryEmail"
             label="Beneficiary Email"
@@ -74,10 +80,12 @@ export default class CreatePayoutClass extends Vue {
     idempotencyKey: '',
     amount: '0.00',
     destination: '',
+    destinationType: 'wire', // Default to wire
     beneficiaryEmail: '',
   }
 
   required = [(v: string) => !!v || 'Field is required']
+  destinationType = ['wire', 'ach']
   error = {}
   loading = false
   showError = false
@@ -97,7 +105,7 @@ export default class CreatePayoutClass extends Vue {
       amount: amountDetail,
       destination: {
         id: this.formData.destination,
-        type: 'wire',
+        type: this.formData.destinationType,
       },
       metadata: {
         beneficiaryEmail: this.formData.beneficiaryEmail,
