9df99e13652ad2d766521be6cdae1e2b0288a2ed
antiv0 <stjepanantivo.ivica@circle.com>
2022-02-14 11:12:49 -0800

    collecting files

---
+++ lib/navigatorInfo.ts
@@ -0,0 +1,7 @@
+function getIsSafari() {
+    const userAgent = navigator.userAgent;
+    const vendor = navigator.vendor;
+    return userAgent.toLowerCase().includes('safari') && vendor.toLowerCase().includes('apple computer');
+  }
+
+  export { getIsSafari }
