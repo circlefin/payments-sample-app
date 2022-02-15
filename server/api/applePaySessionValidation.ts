cb011f8733e278e47a58c64cc2b08920f52da2ba
antiv0 <stjepanantivo.ivica@circle.com>
2022-02-14 22:57:52 -0800

    add

--- server/api/applePaySessionValidation.ts
+++ server/api/applePaySessionValidation.ts
@@ -1,6 +1,19 @@
 import type { IncomingMessage, ServerResponse } from 'http'
 
 export default async (req: IncomingMessage, res: ServerResponse) => {
-  res.statusCode = 200
-  res.end('Works!')
+    console.log(req.method, req.url, req.headers);
+    
+    let body = '';
+    req.on('data', (chunk) => {
+        body += chunk;
+    });
+    req.on('end', () => {
+        console.log(body);
+        let obj = JSON.parse(body);
+
+        // access obj.myString;
+
+        res.statusCode = 200
+        res.end('Works!')
+    });
 }
