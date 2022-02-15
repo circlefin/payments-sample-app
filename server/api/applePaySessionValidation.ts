b2ff9dcb2cbecc326f9738aecc0ffacdb7d5cc75
antiv0 <stjepanantivo.ivica@circle.com>
2022-02-14 23:09:25 -0800

    commit before experiment

--- server/api/applePaySessionValidation.ts
+++ server/api/applePaySessionValidation.ts
@@ -1,17 +1,30 @@
+import axios from 'axios'
 import type { IncomingMessage, ServerResponse } from 'http'
 
 export default async (req: IncomingMessage, res: ServerResponse) => {
     console.log(req.method, req.url, req.headers);
     
-    let body = '';
+    let appleUrl = '';
     req.on('data', (chunk) => {
-        body += chunk;
+        appleUrl += chunk;
     });
     req.on('end', () => {
-        console.log(body);
-        let obj = JSON.parse(body);
-
+        console.log(appleUrl);
+        // let obj = JSON.parse(appleUrl);
         // access obj.myString;
+
+        var response = axios.post(
+            appleUrl,
+            {
+                merchantIdentifier: 'your.apple.merchant.id',
+                domainName: 'yourdomain.com',
+                displayName: 'Your Shop Name',
+            },
+            {
+                httpsAgent,
+            }
+        );
+        res.send(response.data);
 
         res.statusCode = 200
         res.end('Works!')
