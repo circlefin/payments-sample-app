4f362d5d6d1485d2021411d594745f796ca89a13
antiv0 <stjepanantivo.ivica@circle.com>
2022-02-17 06:26:51 -0800

    client secrets cannot be added easily

--- server-middleware/applePaySettings.ts
+++ server-middleware/applePaySettings.ts
@@ -1,8 +1,6 @@
 const applePaySecretsAreSet = !(
   process.env.APPLE_PAY_CERTIFICATE == null ||
-  process.env.APPLE_PAY_PRIVATE_KEY == null ||
-  process.env.APPLE_PAY_SESSION_VALIDATE_URL == null ||
-  process.env.APPLE_PAY_PERFORM_PAY_URL == null
+  process.env.APPLE_PAY_PRIVATE_KEY == null
 )
 
 const merchantIdentityCertificate: string = applePaySecretsAreSet
@@ -12,17 +10,4 @@ const merchantIdentityKey: string = applePaySecretsAreSet
   ? process.env.APPLE_PAY_PRIVATE_KEY!
   : ''
 
-const sessionValidateUrl: string = applePaySecretsAreSet
-  ? process.env.APPLE_PAY_SESSION_VALIDATE_URL!
-  : ''
-
-const payUrl: string = applePaySecretsAreSet
-  ? process.env.APPLE_PAY_PERFORM_PAY_URL!
-  : ''
-
-export {
-  merchantIdentityCertificate,
-  merchantIdentityKey,
-  sessionValidateUrl,
-  payUrl,
-}
+export { merchantIdentityCertificate, merchantIdentityKey }
