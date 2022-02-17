a18f1ec6ff9054f520925f9e7c25c6a3c0c8a3bb
antiv0 <stjepanantivo.ivica@circle.com>
2022-02-16 23:49:14 -0800

    leave the form out of this keep it at 0.5 default pay

--- pages/debug/payments/applepay/create.vue
+++ pages/debug/payments/applepay/create.vue
@@ -1,20 +1,16 @@
 <template>
-  <div>
-    <v-text-field v-model="amountFormatted" :rules="rules" />
-
-    <v-btn
-      elevation="24"
-      dark
-      class="apple-pay-button apple-pay-button-text-pay"
-      v-if="isApplePayAvailable"
-      @click.prevent="doApplePay"
-    >
-    </v-btn>
-  </div>
+  <v-btn
+    elevation="24"
+    dark
+    class="apple-pay-button apple-pay-button-text-pay"
+    v-if="isApplePayAvailable"
+    @click.prevent="doApplePay"
+  >
+  </v-btn>
 </template>
 
 <script lang="ts">
-import { Component, Prop, Watch, Vue } from 'nuxt-property-decorator'
+import { Component, Vue } from 'nuxt-property-decorator'
 import {
   startApplePaySession,
   default_config,
@@ -22,56 +18,6 @@ import {
 } from '@/lib/applePay'
 @Component({})
 export default class CreateApplePayVue extends Vue {
-  @Prop({ type: String, default: '' }) value!: string
-
-  amountFormatted: string = '0.00'
-
-  ruleFunctions = {
-    positive: (v: string) => {
-      return parseFloat(v) > 0 || 'Please enter a positive amount'
-    },
-    isCurrency: (v: string) => {
-      const amount = v.trim()
-      return (
-        /^[0-9]+(.[0-9]{1,2})?$/.test(amount) || 'Please enter valid amount'
-      )
-    },
-    isNumber: (v: string) => {
-      return !isNaN(parseInt(v)) || 'Please enter valid amount'
-    },
-    isRequired: (v: string) => {
-      return v.trim() !== '' || 'Please enter an amount'
-    },
-  }
-
-  get rules() {
-    return [
-      this.ruleFunctions.isRequired,
-      this.ruleFunctions.isNumber,
-      this.ruleFunctions.isCurrency,
-      this.ruleFunctions.positive,
-    ]
-  }
-
-  @Watch('amountFormatted', { immediate: true })
-  amountFormattedChange(value: string) {
-    this.amountFormatted = this.format(value)
-    this.$emit('input', this.amountFormatted)
-  }
-
-  @Watch('value', { immediate: true })
-  valueChange(value: string) {
-    this.amountFormatted = this.format(value)
-  }
-
-  format(value: string) {
-    if (!value) {
-      return ''
-    }
-
-    return value
-  }
-
   isApplePayAvailable: boolean = applePayAvailable()
 
   async doApplePay() {
