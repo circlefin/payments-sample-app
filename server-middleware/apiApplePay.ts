1c5aa3047ebf08ce36eb786a8d979ee0fdc9789e
Stjepan Antivo Ivica <stjepanantivo.ivica@circle.com>
2022-03-01 04:22:07 -0800

    Update Apple Pay in Staging (#166)
    * init
    
    * i

--- server-middleware/apiApplePay.ts
+++ server-middleware/apiApplePay.ts
@@ -15,40 +15,61 @@ const DISPLAY_NAME = 'Circle Apple Pay'
 // 1) validate session, requested by client
 // 2) pay with apple token
 
-// Validates Apple Pay Session, requested by client by providing applePayUrl at which we validate
+// Validates Apple Pay Session, requested by client by providing appleUrl at which we validate
 // responds with validation to client
-app.post('/validate', async (req, res) => {
-  const { applePayUrl } = req.body
-  console.log(applePayUrl)
-  const httpsAgent = new https.Agent({
-    rejectUnauthorized: false,
-    cert: merchantIdentityCertificate, // pem apple cert
-    key: merchantIdentityKey, // key apple
+app.post('/validate', (req, res) => {
+  req.on('data', (data) => {
+    // data is in byte array so first transform it to string and then parse it to object, and then take it's property appleUrl
+    const { appleUrl } = JSON.parse(data.toString())
+    console.log(JSON.parse(data.toString()).appleUrl)
+
+    const httpsAgent = new https.Agent({
+      rejectUnauthorized: false,
+      cert: merchantIdentityCertificate, // pem apple cert
+      key: merchantIdentityKey, // key apple
+    })
+    axios
+      .post(
+        appleUrl,
+        {
+          merchantIdentifier: MERCHANT_IDENTIFIER,
+          domainName: DOMAIN_NAME,
+          displayName: DISPLAY_NAME,
+        },
+        {
+          httpsAgent,
+        }
+      )
+      .then((a) => {
+        console.log('Successfully validated apple pay session')
+        // return the json received from Apple Pay server unmodified
+        res.send(a.data)
+      })
+      .catch((a) => {
+        res.send({ data: null })
+        console.log('Error occured during session validation')
+        console.log(a.message)
+        console.log(a.response.status)
+        console.log(a.response.data)
+        console.log(a.response.headers)
+      })
   })
-  const response = await axios.post(
-    applePayUrl,
-    {
-      merchantIdentifier: MERCHANT_IDENTIFIER,
-      domainName: DOMAIN_NAME,
-      displayName: DISPLAY_NAME,
-    },
-    {
-      httpsAgent,
-    }
-  )
-  // return the json received from Apple Pay server unmodified
-  res.send(response.data)
 })
 
 // after client recieves session validation, client provides apple pay token which we use to hit EFT endpoint
 app.post('/pay', (req, res) => {
-  const { data } = req.body
+  req.on('data', (data) => {
+    // data is in byte array so first transform it to string and then parse it to object, and then take it's property details
+    const details: ApplePayJS.ApplePayPayment = JSON.parse(
+      data.toString()
+    ).details
 
-  console.log(data)
-  // TODO send to processing endpoint once endpoint is ready
+    console.log(JSON.stringify(details))
+    // soon will update with call to wallets API endpoint
 
-  res.send({
-    approved: true,
+    res.send({
+      approved: true,
+    })
   })
 })
 
