445837730405d4b58828f5fa2705cf6736a3026f
Eliza Lucas <eliza.lucas@circle.com>
2022-03-07 16:47:35 -0500

    use googlepay secrets in sampleapp

--- lib/googlePay.ts
+++ lib/googlePay.ts
@@ -3,6 +3,9 @@ import PaymentDataRequest = google.payments.api.PaymentDataRequest
 import ButtonOptions = google.payments.api.ButtonOptions
 import IsReadyToPayResponse = google.payments.api.IsReadyToPayResponse
 import IsReadyToPayPaymentMethodSpecification = google.payments.api.IsReadyToPayPaymentMethodSpecification
+import PaymentMethodTokenizationSpecification = google.payments.api.PaymentMethodTokenizationSpecification
+import TransactionInfo = google.payments.api.TransactionInfo
+import Environment = google.payments.api.Environment
 
 const DEFAULT_CONFIG = {
   apiVersion: 2,
@@ -25,13 +28,27 @@ const DEFAULT_CONFIG = {
       gatewayMerchantId: 'YOUR_PUBLIC_KEY',
     },
   },
-  transactionInfo: {
+  transactionInfo: <TransactionInfo>{
     currencyCode: 'USD',
     countryCode: 'US',
     totalPriceStatus: 'FINAL',
-    totalPrice: '12.00',
     checkoutOption: 'COMPLETE_IMMEDIATE_PURCHASE',
   },
+  environment: <Environment>'TEST', // TODO: change to 'PRODUCTION' after completing testing
+}
+
+interface PaymentToken {
+  protocolVersion: string
+  signature: string
+  intermediateSigningKey: Object
+  signedMessage: string
+}
+
+interface PaymentRequestConfig {
+  amount: string
+  merchantId: string
+  merchantName: string
+  checkoutKey: string
 }
 
 function getIsReadyToPayRequest() {
@@ -43,43 +60,51 @@ function getIsReadyToPayRequest() {
   return isReadyToPayRequest
 }
 
-const paymentDataRequest: PaymentDataRequest = {
-  apiVersion: 2,
-  apiVersionMinor: 0,
-  merchantInfo: {
-    merchantId: '12345678901234567890',
-    merchantName: 'Example Merchant',
-  },
-  allowedPaymentMethods: [
-    {
-      type: 'CARD',
-      parameters: {
-        allowedAuthMethods: ['PAN_ONLY', 'CRYPTOGRAM_3DS'],
-        allowedCardNetworks: ['MASTERCARD', 'VISA'],
-      },
-      tokenizationSpecification: {
-        type: 'PAYMENT_GATEWAY',
-        parameters: {
-          gateway: 'checkoutltd',
-          gatewayMerchantId: 'YOUR_PUBLIC_KEY',
-        },
-      },
-    },
-  ],
-  transactionInfo: {
-    currencyCode: 'USD',
-    countryCode: 'US',
-    totalPriceStatus: 'FINAL',
-    totalPrice: '12.00',
-    checkoutOption: 'COMPLETE_IMMEDIATE_PURCHASE',
-  },
-}
+function getPaymentDataRequest(config: PaymentRequestConfig) {
+  const amount = config.amount === null ? '0.00' : config.amount
+  const id =
+    config.merchantId === null
+      ? DEFAULT_CONFIG.merchantInfo.merchantId
+      : config.merchantId
+  const name =
+    config.merchantName === null
+      ? DEFAULT_CONFIG.merchantInfo.merchantName
+      : config.merchantName
+  const checkoutKey =
+    config.checkoutKey === null
+      ? DEFAULT_CONFIG.tokenizationSpecification.parameters.gatewayMerchantId
+      : config.checkoutKey
 
-interface PaymentToken {
-  protocolVersion: string
-  signature: string
-  intermediateSigningKey: Object
-  signedMessage: string
+  const paymentDataRequest: PaymentDataRequest = {
+    apiVersion: DEFAULT_CONFIG.apiVersion,
+    apiVersionMinor: DEFAULT_CONFIG.apiVersionMinor,
+    merchantInfo: {
+      merchantId: id,
+      merchantName: name,
+    },
+    allowedPaymentMethods: [
+      {
+        type: DEFAULT_CONFIG.allowedPaymentMethods.type,
+        parameters: DEFAULT_CONFIG.allowedPaymentMethods.parameters,
+        tokenizationSpecification: <PaymentMethodTokenizationSpecification>{
+          type: DEFAULT_CONFIG.tokenizationSpecification.type,
+          parameters: {
+            gateway:
+              DEFAULT_CONFIG.tokenizationSpecification.parameters.gateway,
+            gatewayMerchantId: checkoutKey,
+          },
+        }, // TODO: get merchantID from SSM
+      },
+    ],
+    transactionInfo: {
+      currencyCode: DEFAULT_CONFIG.transactionInfo.currencyCode,
+      countryCode: DEFAULT_CONFIG.transactionInfo.countryCode,
+      totalPriceStatus: DEFAULT_CONFIG.transactionInfo.totalPriceStatus,
+      totalPrice: amount,
+      checkoutOption: DEFAULT_CONFIG.transactionInfo.checkoutOption,
+    },
+  }
+  return paymentDataRequest
 }
 
 let paymentsClient: any = null
@@ -87,10 +112,15 @@ let paymentsClient: any = null
 function getGooglePaymentsClient() {
   if (paymentsClient === null) {
     paymentsClient = new google.payments.api.PaymentsClient({
-      environment: 'TEST', // TODO: get real environment once implementation is finished
+      environment: DEFAULT_CONFIG.environment,
     })
   }
   return paymentsClient
+}
+
+function addGooglePayButton(buttonOptions: ButtonOptions) {
+  const button = paymentsClient.createButton(buttonOptions)
+  document.getElementById('google-pay-button')?.append(button)
 }
 
 function onGooglePayLoaded(buttonOptions: ButtonOptions) {
@@ -99,8 +129,7 @@ function onGooglePayLoaded(buttonOptions: ButtonOptions) {
     .isReadyToPay(getIsReadyToPayRequest())
     .then(function (response: IsReadyToPayResponse) {
       if (response.result) {
-        const button = paymentsClient.createButton(buttonOptions)
-        document.getElementById('google-pay-button')?.append(button)
+        addGooglePayButton(buttonOptions)
       }
     })
     .catch(function (err: any) {
@@ -111,6 +140,8 @@ function onGooglePayLoaded(buttonOptions: ButtonOptions) {
 export {
   onGooglePayLoaded,
   getGooglePaymentsClient,
-  paymentDataRequest,
+  getPaymentDataRequest,
   PaymentToken,
+  PaymentRequestConfig,
+  DEFAULT_CONFIG,
 }
