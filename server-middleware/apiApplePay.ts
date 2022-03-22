8cd2753bfcab2ed9e8daac156999e8d6695ef9b3
Eliza Lucas <eliza.lucas@circle.com>
2022-03-22 18:15:25 -0400

    add domain name

--- server-middleware/apiApplePay.ts
+++ server-middleware/apiApplePay.ts
@@ -63,6 +63,7 @@ app.post('/validate', (req, res) => {
           responseHeaders: a.response.headers,
           appleUrl,
           displayName: DISPLAY_NAME,
+          domainName,
         })
       })
   })
