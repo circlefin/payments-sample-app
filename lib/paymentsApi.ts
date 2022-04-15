a692f0cbf7ff492482da3bc29bb0026664e4b9f0
priyamdsuthar <priyamdipak.suthar@circle.com>
2022-04-15 09:49:07 -0700

    [BRAAV-9510] - Add support for filtering through Payment Status for reversals (#214)
    * Add support for filtering through Payment Status for reversals
    
    * Remove unused filter status
    
    * Fix lint issues

--- lib/paymentsApi.ts
+++ lib/paymentsApi.ts
@@ -191,6 +191,7 @@ function getBalance() {
 
 /**
  * Get reversals
+ * @param {String} status
  * @param {String} from
  * @param {String} to
  * @param {String} pageBefore
@@ -198,6 +199,7 @@ function getBalance() {
  * @param {String} pageSize
  */
 function getReversals(
+  status: string,
   from: string,
   to: string,
   pageBefore: string,
@@ -205,6 +207,7 @@ function getReversals(
   pageSize: string
 ) {
   const queryParams = {
+    status: nullIfEmpty(status),
     from: nullIfEmpty(from),
     to: nullIfEmpty(to),
     pageBefore: nullIfEmpty(pageBefore),
