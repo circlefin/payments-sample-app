9a4448a959199e16a57735352671d726e92d6ff0
Eliza Lucas <eliza.lucas@circle.com>
2022-06-30 07:58:01 -0400

    add comment to endpoint

--- server-middleware/apiGooglePay.ts
+++ server-middleware/apiGooglePay.ts
@@ -3,6 +3,8 @@ import { checkoutKey, merchantId, merchantName } from './googlePaySecrets'
 
 const app = express()
 
+// This endpoint to expose the following values is to move forward with an internal milestone, but this is not the recommended approach for others implementing google pay
+// TODO: refactor google pay to initiate payment from the backend
 app.get('/values', (_, res) => {
   res.send({ merchantId, merchantName, checkoutKey })
 })
