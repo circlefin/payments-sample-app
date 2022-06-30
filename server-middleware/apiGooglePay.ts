ec2610b5e9f2e25b283f8af608de4bb241baee35
Eliza Lucas <eliza.lucas@circle.com>
2022-06-30 18:56:27 -0400

    temporarily use apple pay stored secret

--- server-middleware/apiGooglePay.ts
+++ server-middleware/apiGooglePay.ts
@@ -1,12 +1,13 @@
 import express from 'express'
-import { checkoutKey, merchantId, merchantName } from './googlePaySecrets'
+import { merchantId, merchantName } from './googlePaySecrets'
+import { payfacMerchantIdentityKey } from './applePaySettings'
 
 const app = express()
 
 // This endpoint to expose the following values is to move forward with an internal milestone, but this is not the recommended approach for others implementing google pay
 // TODO: refactor google pay to initiate payment from the backend
 app.get('/values', (_, res) => {
-  res.send({ merchantId, merchantName, checkoutKey })
+  res.send({ merchantId, merchantName, checkoutKey: payfacMerchantIdentityKey })
 })
 
 export default {
