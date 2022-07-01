f3c0c0012294c831bb7260d89cfbdb3424d608b6
Eliza Lucas <eliza.lucas@circle.com>
2022-07-01 12:18:38 -0400

    revert secrets

--- server-middleware/apiGooglePay.ts
+++ server-middleware/apiGooglePay.ts
@@ -1,13 +1,12 @@
 import express from 'express'
-import { merchantId, merchantName } from './googlePaySecrets'
-import { payfacMerchantIdentityKey } from './applePaySettings'
+import { merchantId, merchantName, checkoutKey } from './googlePaySecrets'
 
 const app = express()
 
 // This endpoint to expose the following values is to move forward with an internal milestone, but this is not the recommended approach for others implementing google pay
 // TODO: refactor google pay to initiate payment from the backend
 app.get('/values', (_, res) => {
-  res.send({ merchantId, merchantName, checkoutKey: payfacMerchantIdentityKey })
+  res.send({ merchantId, merchantName, checkoutKey })
 })
 
 export default {
