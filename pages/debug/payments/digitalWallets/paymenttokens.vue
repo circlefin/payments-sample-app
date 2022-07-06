e8ca95d73d4af8a420740cd514b247ef0d52fedc
Eliza Lucas <eliza.lucas@circle.com>
2022-07-06 13:23:51 -0400

    add form to edit token data for debugging (#236)

--- pages/debug/payments/digitalWallets/paymenttokens.vue
+++ pages/debug/payments/digitalWallets/paymenttokens.vue
@@ -90,6 +90,22 @@
               Header: {{ applePayTokenData.header }}
             </p>
           </v-card>
+          <!-- 3 text fields below for debugging only - to be removed once resolved -->
+          <v-text-field
+            v-if="displayGoogleTokens"
+            v-model="googlePayTokenData.protocolVersion"
+            label="Protocol Version"
+          />
+          <v-text-field
+            v-if="displayGoogleTokens"
+            v-model="googlePayTokenData.signature"
+            label="Signature"
+          />
+          <v-text-field
+            v-if="displayGoogleTokens"
+            v-model="googlePayTokenData.signedMessage"
+            label="Signed Message"
+          />
           <v-btn
             v-if="displayGoogleTokens || displayAppleTokens"
             depressed
@@ -307,7 +323,7 @@ export default class ConvertToken extends Vue {
       this.googlePayTokenData.signature = paymentToken.signature
       // Due to the parse earlier, the escaped double quotes were changed. need to change them back.
       this.googlePayTokenData.signedMessage = paymentToken.signedMessage
-        .replace('\\', '\\\\')
+        .replaceAll('\\', '\\\\')
         .replace(/"/g, '\\"')
       this.displayGoogleTokens = true
     }
